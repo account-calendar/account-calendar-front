d500b41b5eb97ad5d21374c54f12aa0362ad0125
tom <godppun@naver.com>
2025-05-22 17:04:55 +0900

    Feat: 디자인 시스템 tailwind 설정 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,64 @@
 export default function Home() {
   return (
     <div>
-      <h1>가계부</h1>
+      <span>가나다라마바사아자차카타파하</span>
+
+      <br />
+
+      <div className="flex items-center gap-x-sm">
+        <span className="text-h-lg-strong">가계부</span>
+        <span className="text-h-md-strong">가계부</span>
+        <span className="text-h-sm-strong">가계부</span>
+        <span className="text-h-xs-strong">가계부</span>
+      </div>
+      <br />
+
+      <div className="flex items-center gap-x-sm">
+        <span className="text-sub-lg-strong">가계부</span>
+        <span className="text-sub-lg-normal">가계부</span>
+        <span className="text-sub-md-strong">가계부</span>
+        <span className="text-sub-md-normal">가계부</span>
+      </div>
+      <br />
+      <div className="flex items-center gap-x-sm">
+        <span className="text-bd-md-normal">가계부</span>
+        <span className="text-bd-md-weak">가계부</span>
+        <span className="text-bd-sm-normal">가계부</span>
+        <span className="text-bd-sm-weak">가계부</span>
+      </div>
+      <br />
+      <div className="flex items-center gap-x-sm">
+        <span className="text-cap-sm-weak">가계부</span>
+      </div>
+      <br />
+      <div className="flex items-center gap-x-sm">
+        <span className="text-lb-lg-strong">가계부</span>
+        <span className="text-lb-lg-normal">가계부</span>
+        <span className="text-lb-md-strong">가계부</span>
+        <span className="text-lb-md-normal">가계부</span>
+      </div>
+      <br />
+      <div className="flex items-center gap-x-sm">
+        <span className="text-btn-xl-strong">가계부</span>
+        <span className="text-btn-lg-strong">가계부</span>
+        <span className="text-btn-md-normal">가계부</span>
+        <span className="text-btn-sm-normal">가계부</span>
+      </div>
+      <br />
+      <div className="flex items-center gap-x-sm">
+        <span className="font-thin">100</span>
+        <span className="font-extralight">200</span>
+        <span className="font-light">300</span>
+        <span className="font-normal">400</span>
+        <span className="font-medium">500</span>
+        <span className="font-semibold">600</span>
+        <span className="font-bold">700</span>
+        <span className="font-extrabold">800</span>
+        <span className="font-black">900</span>
+      </div>
+
+      <div className="rounded-2xl border-normal mb-2xl"></div>
+      <div className="rounded-2xl border-strong"></div>
     </div>
   );
 }
