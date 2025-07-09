2fc0c0a6da1eb612b72e3e1839cb24c2a912a0e7
tom <godppun@naver.com>
2025-07-09 21:39:10 +0900

    fix: 지출 등록 페이지 제거

--- src/app/(web)/budget/page.tsx
+++
@@ -1,8 +0,0 @@
-import { APP_ROUTES } from "@/services/constants/app-routes";
-import { redirect } from "next/navigation";
-
-const Budget = () => {
-  redirect(APP_ROUTES.LOGIN);
-};
-
-export default Budget;
--- src/services/constants/app-routes.ts
+++ src/services/constants/app-routes.ts
@@ -1,5 +1,4 @@
 export const APP_ROUTES = {
   LOGIN: "/",
   MAIN: "/main",
-  BUDGET: "/budget",
 } as const;
